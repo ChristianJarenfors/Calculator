59c2364a0373806eb3879d5c958a7caa9216d104
ChrilleJ <christian.jarenfors@gmail.com>
2016-01-23 16:11:09 +0100

    Some comments and a realisation of the comma problem

--- Kalkylator/Form1.cs
+++ Kalkylator/Form1.cs
@@ -16,46 +16,131 @@ namespace wincalcmini
 {
     public partial class Form1 : Form
     {
+        //Kalkylatorns referens
         Kalkylator Calkisen;
-        MethodInfo[] Methods;
+        // Fångar logg eventsen som kalkylatorn skickar ut
         Logger Loggarn;
+        //Calcinfo innehåller olika variabler double,bool, string och int som kan sparas ner och laddas
+        //Dessutom så använder både Formen och Kalkylatorn samma objekt
         Calcinfo CI = new Calcinfo(0,true,true,false,false,true,0,0,null,null);
+        //En lista för de olika räknesätten för att skriva i den textbox där man ser vad man har gjort
         List<string> CalcMethod;
+        //Strömreferens
         Stream strömmen;
+        //Binaryreferens
         BinaryFormatter binForm;
-        
+        //Formateringsinfo för mina double.Parse operationer
         NumberFormatInfo fmt ;
 
         public Form1()
         {
             InitializeComponent();
-            fmt = new NumberFormatInfo();
 
+            //Instanisering och sättning av min formateringsinfo
+            fmt = new NumberFormatInfo();
             fmt.NegativeSign = "-";
             fmt.NumberDecimalSeparator = ".";
+
+            //Instancing my binaryFormatter
             binForm = new BinaryFormatter();
-            
-            var DLL = Assembly.LoadFrom("WinCalc.dll");
-            var TH = DLL.GetType("WinCalc.Räknare");
-            //var c = Activator.CreateInstance(TH);
-            Methods = TH.GetMethods();
+
+            //Instansiering och sättning av min räknesättslista
             CalcMethod = new List<string>();
             CalcMethod.Add("+");
             CalcMethod.Add("-");
             CalcMethod.Add("*");
             CalcMethod.Add("/");
-            textBoxCalculations.Text = null;
+
+            //Loading previous history and calculations
             LoadHistory();
             LoadCurrentCalc();
+            
+            //Instancing the calkylator with the CalcInfo Loaded above
             Calkisen = new Kalkylator(CI);
+            //Instancing the EventCatcher with the calkylator
             Loggarn = new Logger(Calkisen);
 
         }
-        //bool CIdecimalBoolNotUsed = true;
 
+        //All Numberbutton code is the same and is put in a method with a parameter.
+        //For more specific detail I send you to look at that method where i will comment more
+        //The method for the numberbuttons lies in the begining
 
+        //Besides that the decimal method is uniqe and is at begining of the region
+
+        /*
+        Before I go into detail of all my methods I want to give a  little info on my diffrent boolflags
+        They are these 5:
+        decimalBoolNotUsed: This one indicates whether there is a "." in Indata string already. 
+        inputIsNotDone    : This one indicates whether there have been any input done before.
+        isCalcSelected    : This one indicates whether any calculationoperation has been choosen.
+        Doubleloader      : This one indicates that someone has double clicked in the history and loaded a calculation.
+        firstInput        : This one is an indicator to mark if the indata is the first data  
+        */
         #region Numberbuttons + Decimal
 
+        private void insertNumber(string s)
+        {
+            //This method enters numbers into the output textbox, which in a way also is where the indata comes from.
+            if (!CI.Doubleloader)
+            {
+                //If noone has loaded from history
+                if (CI.inputIsNotDone)
+                {
+                    //and if this is the first input.
+
+                    //then the output textbox is set to "s"
+                    textBoxOutput.Text = s;
+                    //and hence the flag is set to false so the next number won't set but rather add the number to the outputtextbox
+                    CI.inputIsNotDone = false;
+                }
+                //As said above this is where we come come if there have been input already´.
+                else { textBoxOutput.Text += s; }
+            }
+            else
+            {
+                // this only happens if someone has loaded from history.
+                //it is worthwhile noting that the doubleloader flag is set to false if a calculation
+                //method is choosen so this case is if you load and decied to press a numberbutton imediatly
+
+                //Clears the calculation
+                buttonC_Click(null, null);
+                //set the first number to "s" and sets the flag to false as now the first input has been made
+                textBoxOutput.Text = s;
+                CI.inputIsNotDone = false;
+            }
+        }
+        private void buttonDecimal_Click(object sender, EventArgs e)
+        {
+            //Enters the decimal dot
+            if (!CI.Doubleloader)
+            {
+                //If noone has loaded form history
+                if (CI.inputIsNotDone)
+                {
+                    //and no input has been done
+
+                    // the text is set to "0."
+                    textBoxOutput.Text = "0.";
+                    //Boolflag=false since  now we have input and we have a comma
+                    CI.inputIsNotDone = false;
+                    CI.decimalBoolNotUsed = false;
+                }
+                else if (CI.decimalBoolNotUsed)
+                {
+                    //if we already have input(CI.inputIsNotDone=false) and if we have no comma already
+                    textBoxOutput.Text += ".";
+                    CI.decimalBoolNotUsed = false;
+                }
+
+            }
+            else
+            {
+                buttonC_Click(null, null);
+                textBoxOutput.Text = "0.";
+                CI.inputIsNotDone = false;
+            }
+        }
         private void buttonNumber1_Click(object sender, EventArgs e)
         {
             insertNumber("1");
@@ -108,46 +193,8 @@ namespace wincalcmini
             insertNumber("0");
             
         }
-        private void buttonDecimal_Click(object sender, EventArgs e)
-        {
-            if (!CI.Doubleloader)
-            {
-                if (CI.inputIsNotDone)
-                {
-                    textBoxOutput.Text = "0.";
-                    CI.inputIsNotDone = false;
-                }
-                else if (CI.decimalBoolNotUsed)
-                {
-                    textBoxOutput.Text += ".";
-                    CI.decimalBoolNotUsed = false;
-                }
-
-            }
-            else {
-                buttonC_Click(null, null);
-                textBoxOutput.Text = "0.";
-                CI.inputIsNotDone = false;
-            }
-        }
-        private void insertNumber(string s)
-        {
-            if (!CI.Doubleloader)
-            {
-                if (CI.inputIsNotDone)
-                {
-                    textBoxOutput.Text = s;
-                    CI.inputIsNotDone = false;
-                }
-                else { textBoxOutput.Text += s; }
-            }
-            else
-            {
-                buttonC_Click(null, null);
-                textBoxOutput.Text = s;
-                CI.inputIsNotDone = false;
-            }
-        }
+        
+        
         #endregion
 
         #region Operations
@@ -208,18 +255,18 @@ namespace wincalcmini
             {
                 if (CI.isCalcSelected)
                 {
-                    Calkisen.Calculations((operation),double.Parse(textBoxOutput.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo));
+                    Calkisen.Calculations((operation),double.Parse(textBoxOutput.Text,fmt));
                     //CI.currentSum = (double)Methods[operation].Invoke(null, new object[] { CI.currentSum, double.Parse(textBoxOutput.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo) });
                     textBoxOutput.Text = CI.currentSum.ToString();
                 }
                 else
                 {
-                    CI.currentSum = double.Parse(textBoxOutput.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo);
+                    CI.currentSum = double.Parse(textBoxOutput.Text, fmt);
                 }   
             }
             else
             {
-                CI.currentSum = double.Parse(textBoxOutput.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo);
+                CI.currentSum = double.Parse(textBoxOutput.Text, fmt);
             }
         }
         public void CalcTextboxUpdate(int i)
@@ -265,7 +312,7 @@ namespace wincalcmini
             CalcTextboxUpdate(CI.selectedcalcmethod);
             calculate(CI.selectedcalcmethod);
             textBoxCalculations.Text = textBoxCalculations.Text.Remove(textBoxCalculations.Text.Length - 1);
-            listBoxHistory.Items.Add(new HistoryRecord(textBoxCalculations.Text, double.Parse(textBoxOutput.Text)));
+            listBoxHistory.Items.Add(new HistoryRecord(textBoxCalculations.Text, double.Parse(textBoxOutput.Text,fmt)));
             textBoxOutput.Text = "0";
             textBoxCalculations.Text = null;
             CI.Creset();
@@ -300,9 +347,9 @@ namespace wincalcmini
         private void buttonSqrt_Click(object sender, EventArgs e)
         {
             removeDecimal();
-            if (!(double.Parse(textBoxOutput.Text)<0))
+            if (!(double.Parse(textBoxOutput.Text,fmt)<0))
             {
-                textBoxOutput.Text = Calkisen.SquareRot(double.Parse(textBoxOutput.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo)).ToString();
+                textBoxOutput.Text = Calkisen.SquareRot(double.Parse(textBoxOutput.Text,fmt)).ToString();
 
             }
         }
@@ -310,7 +357,7 @@ namespace wincalcmini
         private void buttonSquare_Click(object sender, EventArgs e)
         {
             removeDecimal();
-            textBoxOutput.Text = Calkisen.Power2(double.Parse(textBoxOutput.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo)).ToString();
+            textBoxOutput.Text = Calkisen.Power2(double.Parse(textBoxOutput.Text,fmt)).ToString();
             
         }
 
@@ -321,7 +368,8 @@ namespace wincalcmini
 
         private void buttonPosNeg_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = Calkisen.Invert(double.Parse(textBoxOutput.Text, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo)).ToString();
+            textBoxOutput.Text = Calkisen.Invert(double.Parse(textBoxOutput.Text, fmt)).ToString();
+            CI.inputIsNotDone = false;
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
