8dc1ef76d65f658a38fad6a767d72e206f8abf14
ChrilleJ <christian.jarenfors@gmail.com>
2016-01-23 21:14:35 +0100

    Har gått jätte bra Allt är kommenterat och så Slut för dagen

--- Kalkylator/Kalkylator.cs
+++ Kalkylator/Kalkylator.cs
@@ -7,14 +7,18 @@ using System.Reflection;
 
 namespace wincalcmini
 {
+    //two diffrent delegates
     delegate void NegativeEventThrower(double dd);
     delegate void ClearEventThrower();
     class Kalkylator
     {
+        //Events to be used
         public event NegativeEventThrower Negative;
         public event ClearEventThrower ClearButton;
+        //References to be used through the program
         MethodInfo[] Methods;
         Calcinfo CI;
+        //Constructor sets the CalcInfo  and sets up the MethodInfo[] Methods
         public Kalkylator(Calcinfo CCI)
         {
             CI = CCI;
@@ -22,36 +26,46 @@ namespace wincalcmini
             var TH = DLL.GetType("WinCalc.Räknare");
             Methods = TH.GetMethods();
         }/// <summary>
-        /// 1.Addition 2.Substraction 3.Multiplikation 4.Division
+        /// 2.Addition 3.Substraction 4.Multiplikation 5.Division
         /// </summary>
-        /// <param name="switchis"></param>
+        /// <param name="switchis">Choose between the Arithmetics</param>
+        /// <param name="inputdata">The number to count with</param>
         /// <returns></returns>
         public void Calculations(int switchis,double inputdata)
         {
             double returner = 0;
             switch (switchis)
-            { 
+            {
+                //Addition 
                 case 2:
                     {
                     returner= double.Parse(Methods[(switchis )].Invoke(null, new object[] { CI.currentSum, inputdata }).ToString());
                         break;
                     }
-            case 3:
-                {
+                //Substraction
+                case 3:
+                    {
                     returner = double.Parse(Methods[(switchis)].Invoke(null, new object[] { CI.currentSum, inputdata }).ToString());
                         break;
                     }
-            case 4:
-                {
+                //Multiplication
+                case 4:
+                    {
                     returner = double.Parse(Methods[(switchis )].Invoke(null, new object[] { CI.currentSum, inputdata }).ToString());
                         break;
                     }
-            case 5:
-                {
+                //Division
+                case 5:
+                    {
                         if (inputdata == 0)
                         {
-                            returner = 1337;
+                            //This is a joke. Could be set to anything. in any case dividing by 0 is an infinity
+                            //soooo...  ...
+                            //returner = 1337;
+
+                            CI.Creset();
                         }
+
                         else
                         {
                             returner = double.Parse(Methods[(switchis)].Invoke(null, new object[] { CI.currentSum, inputdata }).ToString());
@@ -63,32 +77,41 @@ namespace wincalcmini
                     break;
                 }
             }
+            //If the answer is less then 0 throw the Negative event with returner as parameter
             if (returner < 0)
             {
                 Negative(returner);
             }
+            //finaly set CI.currentsum to the value
             CI.currentSum = returner;
         }
+        //Gets Pi from the reflected method
         public double PI()
         {
             return double.Parse(Methods[0].Invoke(null, new object[] { }).ToString());
         }
+        //Gets Euler from the reflected method
         public double Euler()
         {
             return double.Parse(Methods[1].Invoke(null, new object[] { }).ToString());
         }
+        //Calculates the squareroot from the reflected method with the input dd
         public double SquareRot(double dd)
         {
             return double.Parse(Methods[6].Invoke(null, new object[] {dd }).ToString());
         }
+        //Calculates the Power2 from the reflected method with the input dd
         public double Power2(double dd)
         {
             return double.Parse(Methods[7].Invoke(null, new object[] { dd }).ToString());
         }
+        //Inverts the inputdata dd
         public double Invert(double dd)
         {
             return dd * (-1);
         }
+
+        //Runs the clearmethod in Calcinfo and fires the CLearButton event.
         public void ClearKey()
         {
             CI.Creset();
--- Kalkylator/Logger.cs
+++ Kalkylator/Logger.cs
@@ -10,83 +10,47 @@ namespace wincalcmini
 {
     class Logger
     {
+        //References to be used
         Stream flow;
         StreamWriter SW;
-        StreamReader SR;
         Kalkylator Calc;
-        StringBuilder SB;
+        //Constructor that sets up the Kalkylator reference with the the kalkylator object used elsewhere (in Form1)
+        //also adds two Methods to the events in the kalkylator object
         public Logger(Kalkylator calc)
         {
-            
             Calc = calc;
             calc.Negative += NegativeResult;
             calc.ClearButton += ClearPressing;
         }
         public void NegativeResult(double dd)
         {
+            //Opens a stream and sets a streamwriter to it
             flow = File.Open("Logg.txt", FileMode.Append, FileAccess.Write);
             SW = new StreamWriter(flow);
+
+            //Writes the information in the dokument and the number of this instance (dd)
             SW.WriteLine(DateTime.Now + "   Negative number: " + dd);
 
+            //FLush and close the Streamwriter and the stream
             flow.Flush();
-
             SW.Flush();
             SW.Close();
             flow.Close();
-            //flow = File.Open("Logg.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-
-            //SR = new StreamReader(flow);
-            //SB= new StringBuilder(SR.ReadToEnd());
-            //SB.AppendLine(DateTime.Now + "   Negative number: " + dd);
-            //flow.Flush();
-            //SR.Close();
-            //flow.Close();
-            //flow = File.Open("Logg.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            //SW = new StreamWriter(flow);
-            //SW.WriteLine(SB);
-            //flow.Flush();
-
-            //SW.Flush();
-            //SW.Close();
-            //flow.Close();
         }
         public void ClearPressing()
         {
-            //flow = File.Open("Logg.txt", FileMode.Open, FileAccess.Read);
-
-            //SR = new StreamReader(flow);
-            //SB = new StringBuilder(SR.ReadToEnd());
-            //SB.AppendLine(DateTime.Now + "   Canceled Calculation ");
-
-
-            //flow.Flush();
-            //SR.Close();
-            //flow.Close();
-
-            //flow = File.Open("Logg.txt", FileMode.Create, FileAccess.Write);
-            //SW = new StreamWriter(flow);
-            //SW.WriteLine(SB);
-            //flow.Flush();
-
-            //SW.Flush();
-            //SW.Close();
-            //flow.Close();
-
-
-
+            //Opens a stream and sets a streamwriter to it
             flow = File.Open("Logg.txt", FileMode.Append, FileAccess.Write);
             SW = new StreamWriter(flow);
+
+            //Writes the information in the dokument
             SW.WriteLine(DateTime.Now + "   Canceled Calculation ");
-
+            
+            //FLush and close the Streamwriter and the stream
             flow.Flush();
-
             SW.Flush();
             SW.Close();
             flow.Close();
-        }
-        private void Loader()
-        {
-
         }
     }
 }
